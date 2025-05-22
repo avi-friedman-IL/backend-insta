bb2d91736a968abc45d0a092cc23a0acce4e764d
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2025-05-22 16:44:12 +0300

    Update template routes to require admin authentication for add, update, and delete operations

--- api/template/template.routes.js
+++ api/template/template.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { requireAuth } from '../../middlewares/requireAuth.middleware.js';
+import { requireAdmin } from '../../middlewares/requireAuth.middleware.js';
 import { getTemplate, getTemplates, addTemplate, deleteTemplate, updateTemplate } from './template.controller.js';
 
 export const templateRoutes = express.Router();
 
 templateRoutes.get('/', getTemplates);
 templateRoutes.get('/:id', getTemplate);
-templateRoutes.post('/', addTemplate);
-templateRoutes.put('/:id', updateTemplate);
-templateRoutes.delete('/:id', deleteTemplate);
+templateRoutes.post('/', requireAdmin, addTemplate);
+templateRoutes.put('/:id', requireAdmin, updateTemplate);
+templateRoutes.delete('/:id', requireAdmin, deleteTemplate);
