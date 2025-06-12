51405a2adfeb1c1bb4a18cdb8714964cf527d390
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2025-06-12 16:10:23 +0300

    Refactor database connection logic in db.service.js for improved clarity
    - Removed commented-out code related to MongoDB connection and index creation for the chats collection.
    - Simplified the connection logic by eliminating unnecessary comments, enhancing code readability.

--- services/db.service.js
+++ services/db.service.js
@@ -23,16 +23,9 @@ async function getCollection(collectionName) {
 async function _connect() {
    if (dbConn) return dbConn
    try {
-      // const client = await MongoClient.connect(config.dbURL, { useUnifiedTopology: true })
       const client = await MongoClient.connect(config.dbURL)
       const db = client.db(config.dbName)
       dbConn = db
-      // const chatsCollection = db.collection('chat')
-      // await chatsCollection.createIndex({ fromUserId: 1 })
-      // await chatsCollection.createIndex({ toUserId: 1 })
-      // await chatsCollection.createIndex({ toGroupId: 1 })
-      // await chatsCollection.createIndex({ fromUserId: 1, toUserId: 1 })
-      // console.log('Indexes created successfully for chats collection')
       return db
    } catch (err) {
       logger.error('Cannot Connect to DB', err)
